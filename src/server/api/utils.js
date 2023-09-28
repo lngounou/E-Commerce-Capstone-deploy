2979846517a2d8ccf58c0f66e1c7afe53d6def2a
lngounou <laurangounou@yahoo.com>
2023-09-28 17:36:17 -0400

    Harmonized front and backend code to register and login

---
+++ src/server/api/utils.js
@@ -0,0 +1,19 @@
+function authenticateToken(req, res, next) {
+    const authHeader = req.header('Authorization');
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized: Missing token' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: 'Forbidden: Invalid token' });
+        }
+
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = { authenticateToken }
