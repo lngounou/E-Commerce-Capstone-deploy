bab2bad0836bd5a8542c935c54a824b0dc28ecee
scrowley016 <33128271+scrowley016@users.noreply.github.com>
2023-10-18 22:50:41 -0400

    fixed the API perhaps

--- src/server/db/products.js
+++ src/server/db/products.js
@@ -1,13 +1,13 @@
 const { Pool } = require('pg');
 const db = require('./client')
 
-// const pool = new Pool({
-//   user: process.env.USER || 'localhost',
-//   password: process.env.PASSWORD || 'password',
-//   host: 'localhost',
-//   database: 'commerce',
-//   port: 5432,
-// });
+const pool = new Pool({
+  user: process.env.USER || 'localhost',
+  password: process.env.PASSWORD || 'password',
+  host: 'localhost',
+  database: 'commerce',
+  port: 5432,
+});
 
 const createProduct = async (product) => {
   const { name, description, price, img_url } = product;
@@ -17,7 +17,7 @@ const createProduct = async (product) => {
   };
 
   try {
-    const result = await db.query(query);
+    const result = await pool.query(query);
     return result.rows[0];
   } catch (error) {
     throw error;
@@ -26,7 +26,7 @@ const createProduct = async (product) => {
 
 const getProducts = async () => {
   try {
-    const result = await db.query('SELECT * FROM products');
+    const result = await pool.query('SELECT * FROM products');
     return result.rows;
   } catch (error) {
     throw error;
@@ -35,7 +35,7 @@ const getProducts = async () => {
 
 const getProductById = async (productId) => {
   try {
-    const result = await db.query('SELECT * FROM products WHERE id = $1', [productId]);
+    const result = await pool.query('SELECT * FROM products WHERE id = $1', [productId]);
     return result.rows[0];
   } catch (error) {
     throw error;
@@ -50,7 +50,7 @@ const updateProduct = async (productId, updatedProduct) => {
   };
 
   try {
-    const result = await db.query(query);
+    const result = await pool.query(query);
     return result.rows[0];
   } catch (error) {
     throw error;
@@ -59,7 +59,7 @@ const updateProduct = async (productId, updatedProduct) => {
 
 const deleteProduct = async (productId) => {
   try {
-    const result = await db.query('DELETE FROM products WHERE id = $1 RETURNING *', [productId]);
+    const result = await pool.query('DELETE FROM products WHERE id = $1 RETURNING *', [productId]);
     return result.rows[0];
   } catch (error) {
     throw error;
